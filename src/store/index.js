ab41f91215855da9c8a31e232c38752870aa11be
Constantin Lisnic <costelisnic98@gmail.com>
2022-09-29 17:28:32 +0100

    added redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,42 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { apiSlice } from "./apiSlice";
 import settings from "./settingsSlice";
 import fullTable from "./fullTableSlice";
 
-const store = configureStore({
-  reducer: {
-    settings,
-    fullTable,
-    [apiSlice.reducerPath]: apiSlice.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
+const rootReducer = combineReducers({
+  settings,
+  fullTable,
+  [apiSlice.reducerPath]: apiSlice.reducer,
 });
 
-export default store;
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  whitelist: ["settings"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(apiSlice.middleware),
+});
+
+export const persistor = persistStore(store);
